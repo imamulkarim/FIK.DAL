8f7ff32131f21db6fbb73c292e17ea8283d31096
ImamulKarim <imamul.karim@gmail.com>
2019-10-27 21:20:28 +0600

    s

--- FIK.DAL/FIK.DAL/Properties/AssemblyInfo.cs
+++ FIK.DAL/FIK.DAL/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Imamul Karim")]
 [assembly: AssemblyProduct("FIKDBConnector")]
-[assembly: AssemblyCopyright("Copyright © Imamul Karim 2018")]
+[assembly: AssemblyCopyright("Copyright © Imamul Karim 2018-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.20")]
-[assembly: AssemblyFileVersion("1.0.4.20")]
+[assembly: AssemblyVersion("1.0.5.22")]
+[assembly: AssemblyFileVersion("1.0.5.22")]
