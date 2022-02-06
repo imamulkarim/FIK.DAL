e92984c37303b350fbad2bc6a64870dcea11e4c0
raihanM95 <mahmud.raihan@yahoo.com>
2022-02-06 18:44:32 +0600

    added FIK.DAL.PostgreSQL

---
+++ FIK.DAL/FIK.DAL.PostgreSQL/FIK_NoCUDAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIK.DAL.PostgreSQL
+{
+    public class FIK_NoCUDAttribute : Attribute
+    {
+
+    }
+}
